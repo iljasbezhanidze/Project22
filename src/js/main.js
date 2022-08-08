d08991d6401f89257af35edd3790ba54840b7ada
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2022-08-08 14:04:36 +0300

    mob menu

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,11 @@
 // }
 
 //burger menu
-
 const burgerBtn = document.querySelector('.b-burger'),
       burgerMenu = document.querySelector('header nav')
         
+      burgerBtn.onclick = function() {
+        this.classList.toggle('active')
+        burgerMenu.classList.toggle('active')
+      }
+      
