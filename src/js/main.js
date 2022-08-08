e59d2d081c8f1b2296af1f9d3bc4319675b70aa7
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2022-08-09 02:33:11 +0300

    main btns animate

--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // //burger menu
 const burgerBtn = document.querySelector('.b-burger'),
-  burgerMenu = document.querySelector('header nav')
+      burgerMenu = document.querySelector('header nav')
 
 burgerBtn.onclick = function () {
   this.classList.toggle('active')
   burgerMenu.classList.toggle('active')
 }
 
-//menu punkts hover animate(wrpper overflow)
+//menu punkts hover animate(wrapper overflow)
 document.querySelectorAll('[data-roll-animate]').forEach(el => {
   el.parentNode.style.overflowY = "hidden"
 });
+
+//main button animate
+document.querySelectorAll('.b-btn').forEach(el => {
+  let innerText1 = document.createElement('div'),
+      wrapperText = document.createElement('div');
+const btnText = el.innerHTML
+      el.innerHTML = ''
+      wrapperText.className = 'b-btn__inner';
+      innerText1.className = 'b-btn__text';
+      innerText1.setAttribute('data-roll-animate', btnText)
+      el.appendChild(wrapperText);
+      wrapperText.appendChild(innerText1);
+      innerText1.innerHTML = btnText;
+});
+
 
 
